539021b95b15d730a3249ac1fa36c7a5c01e2efb
Facundo <carballofacundo70@gmail.com>
2023-08-16 11:13:03 -0300

    read data from database

--- server/main.go
+++ server/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+
+	"github.com/facundocarballo/task-manager/handlers/db"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -11,5 +15,30 @@ func main() {
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!!!")
+	database, err := sql.Open("mysql", db.DATABASE_DATASOURCE)
+	if err != nil {
+		panic(err)
+	}
+	defer database.Close()
+
+	rows, err := database.Query("SELECT username, email FROM USER")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var username string
+		var email string
+		err := rows.Scan(&username, &email)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Fprintf(w, "Usuario: "+username+"\n")
+		fmt.Fprintf(w, "Email: "+email+"\n")
+	}
+
+	if err := rows.Err(); err != nil {
+		panic(err.Error())
+	}
 }
