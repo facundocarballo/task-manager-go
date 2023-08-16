cd75bda306b01bc11f5ab56d8a524ff6e4dceb67
Facundo <carballofacundo70@gmail.com>
2023-08-16 14:54:38 -0300

    server endpoints

--- server/main.go
+++ server/main.go
@@ -24,8 +24,56 @@ func main() {
 	defer database.Close()
 
 	// Get Endpoints
-	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("get/users", func(w http.ResponseWriter, r *http.Request) {
 		get.GetAllUsers(w, r, database)
+	})
+
+	// Get a specefic user -> get/user?id=10
+	http.HandleFunc("get/user", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	// Get all the categories -> get/user?id=10/categories
+	http.HandleFunc("get/user/categories", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	// Get a specific category -> get/user?id=10/category?id=9
+	http.HandleFunc("get/category/", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	// Post Endpoints
+	http.HandleFunc("/post/auth-user", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	http.HandleFunc("/post/create-user", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	http.HandleFunc("/post/create-task", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	http.HandleFunc("/post/complete-task", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	http.HandleFunc("/post/delete-task", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	http.HandleFunc("/post/delete-user", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	http.HandleFunc("/post/category", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	http.HandleFunc("/post/delete-category", func(w http.ResponseWriter, r *http.Request) {
+
 	})
 
 	// Listen
