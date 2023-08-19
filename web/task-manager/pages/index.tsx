ec7a380dd704e3e628ccf25755c10297d4277cda
Facundo Carballo <carballofacundo70@gmail.com>
2023-08-19 16:20:14 -0300

    nav bar component

--- web/task-manager/pages/index.tsx
+++ web/task-manager/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { HStack, VStack } from '@chakra-ui/react'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { NavBar } from '@/src/components/NavBar';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,11 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <VStack w="full">
-        <HStack w="full">
-
-        </HStack>
-      </VStack>
+      <NavBar />
     </>
   )
 }
---
+++ web/task-manager/src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { HStack, VStack, Box, Spacer, Heading } from "@chakra-ui/react";
+
+export const NavBar = () => {
+    // Attributes
+    // Context
+    // Methods
+    // Component
+    return (
+        <>
+            <VStack w="full">
+                <Box h='10px' />
+                <HStack w="full">
+                    <Box w='10px' />
+                    <Heading>Task Manager</Heading>
+                </HStack>
+            </VStack>
+        </>
+    );
+};
