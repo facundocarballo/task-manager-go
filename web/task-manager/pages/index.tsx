c072e2533af9d48f0ccf934a2158da07a508d829
Facundo Carballo <carballofacundo70@gmail.com>
2023-08-24 10:44:21 -0300

    remove choose category and input task

--- web/task-manager/pages/index.tsx
+++ web/task-manager/pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head';
 import { NavBar } from '@/src/components/NavBar';
-import { InputTask } from '@/src/subpages/InputTask';
 import { TheDivider } from '@/src/components/TheDivider';
-import { Tasks } from '@/src/subpages/Tasks';
 import { Categories } from '@/src/subpages/Categories';
 import { Footer } from '@/src/subpages/Footer';
 import { Spacer, VStack, useColorModeValue } from '@chakra-ui/react';
@@ -18,10 +16,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
-      {/* <TheDivider horizontal={true} /> */}
-      {/* <InputTask /> */}
-      {/* <TheDivider horizontal={true} /> */}
-      {/* <Tasks /> */}
       <TheDivider horizontal={true} />
       <Categories />
       <Spacer />
